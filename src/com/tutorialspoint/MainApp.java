3aa470c00d212ba3890d1ba7c69db97a10efd019
Foysal Ahamed <thisisfoysal@gmail.com>
2018-10-10 11:41:39 +0600

    Spring - Injecting Inner Beans

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,23 +1,12 @@
 package com.tutorialspoint;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
-        World world = (World) context.getBean("world");
-        world.getLanguage();
-        world.getGreetings();
-
-        Bangladesh bangladesh = (Bangladesh) context.getBean("bangladesh");
-        bangladesh.getLanguage();
-        bangladesh.getGreetings();
-        bangladesh.getPrimaryLanguage();
-
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
-        context.registerShutdownHook();
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        TextEditor te = (TextEditor) context.getBean("textEditor");
+        te.spellCheck();
     }
 }
---
+++ src/com/tutorialspoint/SpellChecker.java
@@ -0,0 +1,11 @@
+package com.tutorialspoint;
+
+public class SpellChecker {
+    private SpellChecker() {
+        System.out.println("Inside SpellChecker constructor.");
+    }
+
+    public void checkSpelling() {
+        System.out.println("Inside checkSpelling.");
+    }
+}
